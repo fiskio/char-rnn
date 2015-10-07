05312eba93d21911030650913ad41ae0b9edbbd9
Marco Fiscato <marco@swifkey.com>
2015-10-07 12:36:49 +0100

    better emoji util

--- util/emoji.lua
+++ util/emoji.lua
@@ -1,10 +1,11 @@
+local stx = require 'pl.stringx'
 local emoji = {}
 
 emoji.all_emoji = {}
-for line in io.lines('util/all_emoji.txt') do
-   emoji.all_emoji[line] = true
+for line in io.lines('all_emoji.txt') do
+   emoji.all_emoji[stx.strip(line)] = true
 end
-
+print(emoji)
 function emoji.isEmoji(token)
    return emoji.all_emoji[token] and true or false
 end
