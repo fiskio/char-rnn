eb81df08ddca62d67ce786672ca6f71cbd824463
Arpi Vezer <arpi@swiftkey.com>
2015-10-09 11:46:38 +0100

    projection/tanh layer for CBOW

--- model/CBOW.lua
+++ model/CBOW.lua
@@ -15,8 +15,8 @@ function CBOW.create(vocab_size, context_size, output_class_size, hidden_size, e
    local inputs = {nn.Identity()()}
    local encoder = nn.LookupTable(vocab_size, emb_size)(inputs[1]):annotate{label='Embedding lookup'}
    local sum = nn.Transpose()(nn.CAddTable()(nn.SplitTable(2)(nn.Transpose()(encoder))))
-   local projection = nn.Linear(emb_size, hidden_size)(sum)
-   local logsoft = LSM.lsm(output_class_size, emb_size, emb_size, dropout, hsm, encoder)(sum)
+   local projection = nn.Tanh()(nn.Linear(emb_size, hidden_size)(sum))
+   local logsoft = LSM.lsm(output_class_size, hidden_size, emb_size, dropout, hsm, encoder)(projection)
    local outputs = {logsoft}
    return nn.gModule(inputs, outputs)
 end
